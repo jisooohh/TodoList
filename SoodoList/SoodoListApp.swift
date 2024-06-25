5924b5c9d677a7c31c69014ee21968a4b4a331a8
jisooohh <a01031141317@gmail.com>
2024-06-25 21:30:12 +0900

    Initial Commit

---
+++ SoodoList/SoodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoodoListApp.swift
+//  SoodoList
+//
+//  Created by 홍지수 on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SoodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
